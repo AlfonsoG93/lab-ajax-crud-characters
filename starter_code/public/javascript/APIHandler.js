d84dad980e52b1fc86b1efa383b2908fa426facf
Alfonso Moncada <alfonsomcd93@gmail.com>
2017-11-29 21:46:23 -0500

    Done

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,141 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  getFullList() {
+    $.ajax({
+        method: "GET",
+        url: `${this.BASE_URL}/characters`
+      })
+      .then((apiResults) => {
+        console.log("SUCESS!");
+        console.log(apiResults);
+
+        const fullList = apiResults;
+
+        fullList.forEach((oneCharacter) => {
+          let char = $(`
+           <div class="character-info">
+                <div class="name"> ${oneCharacter.name}</div>
+                <div class="charId"> ${oneCharacter.id}</div>
+                <div class="occupation"> ${oneCharacter.occupation}</div>
+                <div class="debt"> ${oneCharacter.debt}</div>
+                <div class="weapon"> ${oneCharacter.weapon}</div>   
+            </div>
+          `);
+          $(".characters-container").append(char);
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+
+  getOneRegister(charId) {
+    $.ajax({
+        method: "GET",
+        url: `${this.BASE_URL}/characters/${charId}/`
+      })
+      .then((apiResults) => {
+        console.log("SUCCESS!");
+        console.log(apiResults);
+
+        const oneCharacter = $(`
+        <div class="character-info">
+          <div class="name"> ${apiResults.name}</div>
+          <div class="charId"> ${apiResults.id}</div>
+          <div class="occupation"> ${apiResults.occupation}</div>
+          <div class="debt"> ${apiResults.debt}</div>
+          <div class="weapon"> ${apiResults.weapon}</div>   
+        </div>
+      `);
+        $(".characters-container").append(oneCharacter);
+
+
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+
+  createOneRegister(charName, charJob, charWeapon, charDebt) {
+    $.ajax({
+        method: "POST",
+        url: `${this.BASE_URL}/characters`,
+
+        data: {
+          name: charName,
+          occupation: charJob,
+          weapon: charWeapon,
+          debt: charDebt,
+        }
+      })
+      .then((apiResults) => {
+        console.log("POST Success");
+
+        const newCharHtml = (`
+        <li> Added : <b>${apiResults.name}</b> 
+          ( id ${apiResults.id})
+        </li>
+      `);
+        $(".create-feedback").html(newCharHtml);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+
+  updateOneRegister(charId, charName, charJob, charWeapon, charDebt) {
+    $.ajax({
+        method: "PATCH",
+        url: `${this.BASE_URL}/characters/${charId}/`,
+
+        data: {
+          id: charId,
+          name: charName,
+          occupation: charJob,
+          weapon: charWeapon,
+          debt: charDebt,
+        }
+
+      })
+      .then((apiResults) => {
+        console.log("PATCH Success");
+
+        const updateCharHtml = (`
+        <li> UPDATED : <b>${apiResults.name}</b> 
+          ( id ${apiResults.id})
+        </li>
+      `);
+        $(".create-feedback").html(updateCharHtml);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
 
   }
 
-  getOneRegister () {
+  deleteOneRegister(deleteId) {
+    $.ajax({
 
-  }
+      method: "DELETE",
+      url: `${this.BASE_URL}/characters/${deleteId}/`,
+      data: {}
+    })
+    .then((apiResults) => {
+      console.log("DELETE Success");
 
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
+      const deleteCharHtml = (`
+      <li> DELETE : <b>${apiResults.name}</b> 
+        ( id ${apiResults.id})
+      </li>
+    `);
+      $(".delete-feedback").html(updateCharHtml);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
 
   }
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -2,22 +2,52 @@ const charactersAPI = new APIHandler("http://ih-crud-api.herokuapp.com")
 
 $(document).ready( () => {
   $('#fetch-all').on('click', (e) => {
-
+    charactersAPI.getFullList();
   })
 
   $('#fetch-one').on('click', (e) => {
-
+    event.preventDefault();
+    const id = $("#character-id").val();
+    charactersAPI.getOneRegister(id);
   })
 
   $('#delete-one').on('click', (e) => {
-
+    event.preventDefault();
+    const deleteId = $("#character-id-delete").val();
+    charactersAPI.deleteOneRegister(deleteId);
   })
 
   $('#edit-character-form').on('submit', (e) => {
+    event.preventDefault();
+
+    const charId = $("#chr-id").val();
+    const charName = $("#update-name").val();
+    const charJob = $("#update-occupation").val();
+    const charWeapon = $("#update-weapon").val();
+    const charDebt = $("#update-debt").val();
+
+    charactersAPI.updateOneRegister(charId, charName, charJob, charWeapon, charDebt)
+
+    //clear the inputs again
+    $(".edit-character-form").trigger("reset");
+
 
   })
 
   $('#new-character-form').on('submit', (e) => {
+
+    event.preventDefault();
+    
+            const charName = $("#add-name").val();
+            const charJob = $("#add-occupation").val();
+            const charWeapon = $("#add-weapon").val();
+            const charDebt = $("#add-debt").val();
+
+            charactersAPI.createOneRegister(charName, charJob, charWeapon, charDebt)
+    
+            //clear the inputs again
+            $(".new-character-form").trigger("reset");
+        
 
   })
 })
